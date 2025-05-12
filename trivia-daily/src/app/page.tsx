dd88ee176ea5727c4c0242556aa50a93e6a3fdc2
presslerwill <presslerwill@gmail.com>
2025-05-12 00:48:52 -0400

    Finished errors with supabase project

--- trivia-daily/src/app/page.tsx
+++ trivia-daily/src/app/page.tsx
@@ -3,8 +3,8 @@ import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
 
 export default async function Home() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
+  const cookieStore = cookies()
+  const supabase = await createClient(cookieStore)
 
   const { data: todos } = await supabase.from('todos').select()
 
--- trivia-daily/src/utils/supabase/server.ts
+++ trivia-daily/src/utils/supabase/server.ts
@@ -1,19 +1,19 @@
-
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+export const createClient = async (cookieStore: ReturnType<typeof cookies>) => {
+  const cookieData = await cookieStore;
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
-          return cookieStore.getAll()
+          return cookieData.getAll()
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+            cookiesToSet.forEach(({ name, value, options }) => cookieData.set(name, value, options))
           } catch {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
